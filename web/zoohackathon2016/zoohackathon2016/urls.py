bd39a9ebf8cbdc9adab8afdc6533e3d97e24edc9
Kunal Sarkhel <kksarkhel@bluedevs.net>
2016-10-08 23:07:50 -0400

    Add initial map template

--- web/zoohackathon2016/zoohackathon2016/urls.py
+++ web/zoohackathon2016/zoohackathon2016/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from report.views import ReportViewSet
 
@@ -28,4 +29,5 @@ router.register(r'reports', ReportViewSet)
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(router.urls)),
+    url(r'^$', TemplateView.as_view(template_name='map.html')),
 ]
